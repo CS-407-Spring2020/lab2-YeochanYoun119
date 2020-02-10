a2fc7300d400799805bceaa8765623a651020ea2
Yeochan Youn <yyoun5@wisc.edu>
2020-02-10 14:25:20 -0600

    final commit

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickButton(View view){
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        String str = myTextField.getText().toString();
+
+        goToActivity2(str);
+        //Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+        //Log.i("Info", "Button pressed");
+    }
+
+    public void goToActivity2(String s){
+        Intent intent = new Intent(this, Main2Activity.class);
+        intent.putExtra("message", s);
+        startActivity(intent);
     }
 }
